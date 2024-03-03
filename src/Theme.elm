7a8f7088e4e90924099cbb5e49e9ac4fd8854c44
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-03-03 05:27:24 +0100

    Cleanup

--- src/Theme.elm
+++ src/Theme.elm
@@ -1,12 +1,9 @@
-module Theme exposing (Attribute, Context, Element, button, colors, column, darken, fontSizes, grid, onCtrlEnter, onEnter, padding, row, sizes, spacing, text, textInvariant, wrappedRow)
+module Theme exposing (Attribute, Context, Element, button, colors, column, fontSizes, padding, row, sizes, spacing, text, textInvariant, wrappedRow)
 
-import Color
-import Element.WithContext as Element exposing (Color, el, none, rgb, rgba, shrink)
+import Element.WithContext as Element exposing (Color, rgb, rgba)
 import Element.WithContext.Border as Border
 import Element.WithContext.Font as Font
 import Element.WithContext.Input as Input
-import Html.Events
-import Json.Decode as Decode
 import Translations
 
 
@@ -73,30 +70,6 @@ colors =
     }
 
 
-darken : Color -> Color
-darken =
-    mapHsl (\c -> { c | lightness = 0.8 * c.lightness })
-
-
-type alias Hsla =
-    { hue : Float
-    , saturation : Float
-    , lightness : Float
-    , alpha : Float
-    }
-
-
-mapHsl : (Hsla -> Hsla) -> Color -> Color
-mapHsl f =
-    Element.toRgb
-        >> Color.fromRgba
-        >> Color.toHsla
-        >> f
-        >> Color.fromHsla
-        >> Color.toRgba
-        >> Element.fromRgb
-
-
 row : List (Attribute msg) -> List (Element msg) -> Element msg
 row attrs =
     Element.row (spacing :: attrs)
@@ -110,70 +83,6 @@ wrappedRow attrs =
 column : List (Attribute msg) -> List (Element msg) -> Element msg
 column attrs =
     Element.column (spacing :: attrs)
-
-
-grid : List (Attribute msg) -> List (List (Element msg)) -> Element msg
-grid attrs rows =
-    if List.isEmpty rows then
-        none
-
-    else
-        let
-            toColumn : Int -> Element.Column Context (List (Element msg)) msg
-            toColumn i =
-                { width = shrink
-                , header = none
-                , view =
-                    \x ->
-                        x
-                            |> List.drop i
-                            |> List.head
-                            |> Maybe.withDefault none
-                            |> el [ Element.alignBottom ]
-                }
-
-            columnCount : Int
-            columnCount =
-                List.map List.length rows
-                    |> List.maximum
-                    |> Maybe.withDefault 0
-        in
-        Element.table (spacing :: attrs)
-            { columns = List.map toColumn <| List.range 0 (columnCount - 1)
-            , data = rows
-            }
-
-
-onEnter : msg -> Attribute msg
-onEnter msg =
-    Decode.field "key" Decode.string
-        |> Decode.andThen
-            (\s ->
-                if s == "Enter" then
-                    Decode.succeed msg
-
-                else
-                    Decode.fail "ignored"
-            )
-        |> Html.Events.on "keyup"
-        |> Element.htmlAttribute
-
-
-onCtrlEnter : msg -> Attribute msg
-onCtrlEnter msg =
-    Decode.map2 Tuple.pair
-        (Decode.field "key" Decode.string)
-        (Decode.field "ctrlKey" Decode.bool)
-        |> Decode.andThen
-            (\( s, ctrl ) ->
-                if s == "Enter" && ctrl then
-                    Decode.succeed msg
-
-                else
-                    Decode.fail "ignored"
-            )
-        |> Html.Events.on "keyup"
-        |> Element.htmlAttribute
 
 
 button : List (Attribute msg) -> { onPress : Maybe msg, label : Element msg } -> Element msg
